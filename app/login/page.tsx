929b7dd5ba74ae3afc18fffe143dceef056fdda9
Labinot <ferhatibryamov7@gmail.com>
2026-06-21 12:57:12 +0300

    Fixed Auth bug

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,8 +2,13 @@
 
 import { Button } from "@/components/ui/button"
 import { createClient } from "@/lib/supabase/client"
+import { useUser } from "@/lib/use-user"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 export default function LoginPage() {
+  const { isLoggedIn, loading } = useUser()
+  const router = useRouter()
   const supabase = createClient()
 
   async function handleOAuthLogin(provider: "github" | "google") {
@@ -14,6 +19,12 @@ export default function LoginPage() {
       },
     })
   }
+
+  useEffect(() => {
+    if (!loading && isLoggedIn) {
+      router.push("/")
+    }
+  }, [loading, isLoggedIn])
 
   return (
     <div className="min-h-screen flex items-center justify-center ">
@@ -26,11 +37,13 @@ export default function LoginPage() {
           onClick={() => handleOAuthLogin("github")}
           className="flex items-center justify-center gap-2 border  rounded-lg py-2.5 text-sm font-medium text-zinc-800 hover:bg-zinc-100 transition-colors"
         >
-         
+          <svg viewBox="0 0 24 24" className="size-4" fill="currentColor">
+            <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61-.546-1.385-1.333-1.754-1.333-1.754-1.089-.745.083-.729.083-.729 1.205.084 1.84 1.236 1.84 1.236 1.07 1.835 2.807 1.305 3.492.997.107-.775.418-1.305.762-1.605-2.665-.305-5.467-1.334-5.467-5.93 0-1.31.469-2.38 1.236-3.22-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23A11.5 11.5 0 0 1 12 5.803c1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.91 1.235 3.22 0 4.61-2.807 5.625-5.479 5.921.43.372.823 1.102.823 2.222 0 1.604-.015 2.896-.015 3.286 0 .322.216.696.825.578C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+          </svg>
           Continue with GitHub
         </Button>
 
-        <button
+        <Button
           onClick={() => handleOAuthLogin("google")}
           className="flex items-center justify-center gap-2 border border-zinc-300 rounded-lg py-2.5 text-sm font-medium text-zinc-800 hover:bg-zinc-100 transition-colors"
         >
@@ -41,7 +54,7 @@ export default function LoginPage() {
             <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" />
           </svg>
           Continue with Google
-        </button>
+        </Button>
       </div>
     </div>
   )
